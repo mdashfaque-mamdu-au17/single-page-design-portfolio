497605a7452d18d51efbdb72324e31638fb20897
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-10-01 11:39:32 +0530

    added all the colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-galactic-blue': '#755CDE',
+        'theme-summer-yellow': '#F6A560',
+        'theme-pink': '#F39E9E',
+        'theme-light-red': '#EB7565',
+        'theme-cyan': '#61C4B7',
+        'theme-dark-purple': '#552049',
+        'theme-black': '#030303',
+        'theme-medium-brown': '#7A746E',
+        'theme-light-cream': '#FFF7F0',
+        'theme-background-white': '#F2F2F2',
+      },
+    },
   },
   plugins: [],
 };
