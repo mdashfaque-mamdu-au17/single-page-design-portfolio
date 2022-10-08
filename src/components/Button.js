7277c1aa1cb1da0945365ec9ee0972d212ab92f0
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-10-09 02:30:39 +0530

    refactored button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,20 +4,20 @@ import PropTypes from 'prop-types';
 
 function Button({ type, children }) {
   const commonStyle = () => {
-    return 'w-44 h-11 sm:w-[228px] sm:h-14 rounded-[28px]';
+    return 'w-44 h-11 sm:w-[228px] sm:h-14 rounded-[28px] text-sm sm:text-base leading-7 text-theme-light-cream font-bold transition-all duration-500';
   };
   const applyTypeStyle = () => {
     switch (type) {
       case 'primary':
-        return 'bg-theme-black text-sm sm:text-base leading-7 text-theme-light-cream font-bold transition-all duration-500 hover:bg-theme-galactic-blue';
+        return 'bg-theme-black hover:bg-theme-galactic-blue';
       case 'secondary':
-        return '';
+        return 'bg-theme-light-red hover:bg-theme-summer-yellow';
       default:
         break;
     }
   };
   return (
-    <button className={classNames(commonStyle(), applyTypeStyle())}>
+    <button className={classNames(applyTypeStyle(), commonStyle())}>
       {children}
     </button>
   );
