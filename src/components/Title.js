db59676438feb59a4c747a69530e54bd13b6e11c
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-10-09 23:32:00 +0530

    white heading and para adjusted

--- src/components/Title.js
+++ src/components/Title.js
@@ -8,15 +8,22 @@ function Title({ type, content, styles }) {
         return 'text-theme-black';
       case 'secondaryBlack':
         return 'text-theme-medium-brown';
+      case 'primaryWhite':
+        return 'text-theme-light-cream';
+      case 'secondaryWhite':
+        return 'text-theme-light-cream';
       default:
         break;
     }
   };
   if (type === 'primaryBlack') {
-    return <h1 className={classNames(commonStyle, styles)}>{content}</h1>;
+    return <h1 className={classNames(commonStyle(), styles)}>{content}</h1>;
   }
-  if (type === 'secondaryBlack') {
+  if (type === 'secondaryBlack' || 'secondaryWhite') {
     return <p className={classNames(commonStyle(), styles)}>{content}</p>;
+  }
+  if (type === 'primaryWhite') {
+    return <h1 className={classNames(commonStyle(), styles)}>{content}</h1>;
   }
 }
 
