053a3b7d3ddb7f30c68b72452da3da1e3e021d7b
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-10-13 02:02:28 +0530

    margin auto removed

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CarouselItem({ imageSrc }) {
   return (
-    <div className="w-[270px] h-[180px] rounded-[10px] sm:mx-auto sm:w-[540px] sm:h-[360px]">
+    <div className="w-[270px] h-[180px] rounded-[10px] sm:w-[540px] sm:h-[360px]">
       <img
         src={imageSrc}
         alt=""
